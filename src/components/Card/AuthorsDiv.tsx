bb4ef7c05f56f4dbd1fe759ef86f71f67508c83f
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-03 14:50:12 +0100

    y

--- src/components/Card/AuthorsDiv.tsx
+++ src/components/Card/AuthorsDiv.tsx
@@ -1,33 +1,33 @@
-import React from "react";
+import { S } from "/modules/Delusoire/std/index.js";
 import { Author } from "../../types/marketplace-types";
 
 const AuthorsDiv = (props: {
-  authors: Author[];
+	authors: Author[];
 }) => {
-  // Add a div with author links inside
-  const authorsDiv = (
-    <div className="marketplace-card__authors">
-      { props.authors.map((author, index) => {
-        return (
-          <a
-            title={author.name}
-            className="marketplace-card__author"
-            href={author.url}
-            draggable="false"
-            dir="auto"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={(e) => e.stopPropagation()}
-            key={index}
-          >
-            {author.name}
-          </a>
-        );
-      })}
-    </div>
-  );
+	// Add a div with author links inside
+	const authorsDiv = (
+		<div className="marketplace-card__authors">
+			{props.authors.map((author, index) => {
+				return (
+					<a
+						title={author.name}
+						className="marketplace-card__author"
+						href={author.url}
+						draggable="false"
+						dir="auto"
+						target="_blank"
+						rel="noopener noreferrer"
+						onClick={e => e.stopPropagation()}
+						key={index}
+					>
+						{author.name}
+					</a>
+				);
+			})}
+		</div>
+	);
 
-  return authorsDiv;
+	return authorsDiv;
 };
 
 export default AuthorsDiv;
