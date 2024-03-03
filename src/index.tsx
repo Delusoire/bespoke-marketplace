53833eef1d4c806c46f6217062effced8453ca94
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-03 17:11:29 +0100

    y

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { S, createStorage, createRegistrar, createLogger } from "/modules/Delusoire/std/index.js";
+import { createSettings } from "/modules/Delusoire/std/api/settings.js";
+
+import { NavLink } from "/modules/Delusoire/std/registers/navlink.js";
+import { Module } from "/hooks/module.js";
+import type { Settings } from "/modules/Delusoire/std/api/settings.js";
+
+export let storage: Storage = undefined;
+export let logger: Console = undefined;
+export let settings: Settings = undefined;
+
+export default function (mod: Module) {
+	storage = createStorage(mod);
+	logger = createLogger(mod);
+	settings = createSettings(mod);
+	const registrar = createRegistrar(mod);
+
+	const LazyApp = S.React.lazy(() => import("./app.js"));
+	registrar.register("route", <S.ReactComponents.Route path={"/marketplace/*"} element={<LazyApp />} />);
+
+	registrar.register("navlink", () => <NavLink localizedApp="Marketplace" appRoutePath="/marketplace" icon={ICON} activeIcon={ACTIVE_ICON} />);
+}
