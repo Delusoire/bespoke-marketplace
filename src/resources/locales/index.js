15e5c48584d68adbd8fb7b994ad6c5cf6751b267
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-06 16:28:05 +0100

    y

--- src/resources/locales/index.js
+++ src/resources/locales/index.js
@@ -1,22 +1,13 @@
-import ca from "./ca.json";
-import en from "./en.json";
-import enUS from "./en-US.json";
-import es from "./es.json";
-import fr from "./fr.json";
-import zhTW from "./zh-TW.json";
-import zhCN from "./zh-CN.json";
-import ru from "./ru.json";
-import et from "./et.json";
-import pl from "./pl.json";
+import { fetchJSON } from "/hooks/util.js";
 export default {
-    ca,
-    en,
-    "en-US": enUS,
-    es,
-    fr,
-    ru,
-    "zh-TW": zhTW,
-    "zh-CN": zhCN,
-    et,
-    pl,
+    ca: await fetchJSON("./ca.json"),
+    en: await fetchJSON("./en.json"),
+    "en-US": await fetchJSON("./en-US.json"),
+    es: await fetchJSON("./es.json"),
+    fr: await fetchJSON("./fr.json"),
+    ru: await fetchJSON("./ru.json"),
+    "zh-TW": await fetchJSON("./zh-TW.json"),
+    "zh-CN": await fetchJSON("./zh-CN.json"),
+    et: await fetchJSON("./et.json"),
+    pl: await fetchJSON("./pl.json"),
 };
