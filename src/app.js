c585e648750ea864f06a0edc798b83f39fddf65e
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-12 18:38:18 +0000

    y

--- src/app.js
+++ src/app.js
@@ -1,27 +1,18 @@
 import { S } from "/modules/Delusoire/stdlib/index.js";
 import Marketplace from "./pages/Marketplace.js";
 import ModulePage from "./pages/Module.js";
-export default function () {
-	const match = S.useMatch("/marketplace/:murl");
-	const murl = match?.params?.murl;
-	return /*#__PURE__*/ S.React.createElement(
-		"div",
-		{
-			id: "stats-app",
-		},
-		/*#__PURE__*/ S.React.createElement(
-			S.ReactComponents.Routes,
-			null,
-			/*#__PURE__*/ S.React.createElement(S.ReactComponents.Route, {
-				path: "/",
-				element: /*#__PURE__*/ S.React.createElement(Marketplace, null),
-			}),
-			/*#__PURE__*/ S.React.createElement(S.ReactComponents.Route, {
-				path: ":murl",
-				element: /*#__PURE__*/ S.React.createElement(ModulePage, {
-					murl: murl,
-				}),
-			}),
-		),
-	);
+export default function() {
+    const match = S.useMatch("/marketplace/:murl");
+    const murl = match?.params?.murl;
+    return /*#__PURE__*/ S.React.createElement("div", {
+        id: "stats-app"
+    }, /*#__PURE__*/ S.React.createElement(S.ReactComponents.Routes, null, /*#__PURE__*/ S.React.createElement(S.ReactComponents.Route, {
+        path: "/",
+        element: /*#__PURE__*/ S.React.createElement(Marketplace, null)
+    }), /*#__PURE__*/ S.React.createElement(S.ReactComponents.Route, {
+        path: ":murl",
+        element: /*#__PURE__*/ S.React.createElement(ModulePage, {
+            murl: murl
+        })
+    })));
 }
--- src/components/ModuleCard/AuthorsDiv.js
+++ src/components/ModuleCard/AuthorsDiv.js
@@ -1,26 +1,16 @@
 import { S } from "/modules/Delusoire/stdlib/index.js";
-export default function ({ authors }) {
-	return /*#__PURE__*/ S.React.createElement(
-		"div",
-		{
-			className: "",
-		},
-		authors.map((author, index) =>
-			/*#__PURE__*/ S.React.createElement(
-				"a",
-				{
-					title: author,
-					className: "marketplace-card__author",
-					href: `https://github.com/${author}`,
-					draggable: "false",
-					dir: "auto",
-					target: "_blank",
-					rel: "noopener noreferrer",
-					onClick: e => e.stopPropagation(),
-					key: index,
-				},
-				author,
-			),
-		),
-	);
+export default function({ authors }) {
+    return /*#__PURE__*/ S.React.createElement("div", {
+        className: ""
+    }, authors.map((author, index)=>/*#__PURE__*/ S.React.createElement("a", {
+            title: author,
+            className: "marketplace-card__author",
+            href: `https://github.com/${author}`,
+            draggable: "false",
+            dir: "auto",
+            target: "_blank",
+            rel: "noopener noreferrer",
+            onClick: (e)=>e.stopPropagation(),
+            key: index
+        }, author)));
 }
