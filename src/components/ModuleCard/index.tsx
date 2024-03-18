cd1d587f8b26c7cc2e68cdab97293673246bc9f8
harbassan <harbassan@hotmail.com>
2024-03-18 20:21:10 +1300

    format with biome

--- src/components/ModuleCard/index.tsx
+++ src/components/ModuleCard/index.tsx
@@ -9,144 +9,144 @@ import Dropdown from "/modules/Delusoire/stdlib/lib/components/Dropdown.js";
 const History = S.Platform.getHistory();
 
 interface UseMetaSelectorOpts {
-    metaURL: string;
-    setMetaURL: (metaURL: string) => void;
-    metaURLList: string[];
+	metaURL: string;
+	setMetaURL: (metaURL: string) => void;
+	metaURLList: string[];
 }
 
 const useMetaSelector = ({ metaURL, setMetaURL, metaURLList }: UseMetaSelectorOpts) => {
-    const prettifyMeta = (metaURL: string) => {
-        const moduleURL = metaURL.replace(/\/metadata\.json$/, "");
-        try {
-            const url = new URL(moduleURL);
-            switch (url.hostname) {
-                case "raw.githubusercontent.com": {
-                    // return `@github: ${url.pathname}`;
-                    return `@github`;
-                }
-            }
-        } catch (e) {
-            // const match = moduleURL.match(/^\/modules(?<modulePath>\/.*)$/);
-            // const { modulePath } = match.groups ?? {};
-            // return `@local: ${modulePath}`;
-            return `@local`;
-        }
+	const prettifyMeta = (metaURL: string) => {
+		const moduleURL = metaURL.replace(/\/metadata\.json$/, "");
+		try {
+			const url = new URL(moduleURL);
+			switch (url.hostname) {
+				case "raw.githubusercontent.com": {
+					// return `@github: ${url.pathname}`;
+					return `@github`;
+				}
+			}
+		} catch (e) {
+			// const match = moduleURL.match(/^\/modules(?<modulePath>\/.*)$/);
+			// const { modulePath } = match.groups ?? {};
+			// return `@local: ${modulePath}`;
+			return `@local`;
+		}
 
-        return moduleURL;
-    };
+		return moduleURL;
+	};
 
-    const options = Object.fromEntries(metaURLList.map(metaURL => [metaURL, prettifyMeta(metaURL)] as const)) as { [K in string]: K };
-    console.log(options);
+	const options = Object.fromEntries(metaURLList.map(metaURL => [metaURL, prettifyMeta(metaURL)] as const)) as { [K in string]: K };
+	console.log(options);
 
-    const dropdown = (
-        <div className="min-w-fit">
-            <Dropdown options={options} activeOption={metaURL} onSwitch={metaURL => setMetaURL(metaURL)} />
-        </div>
-    );
+	const dropdown = (
+		<div className="min-w-fit">
+			<Dropdown options={options} activeOption={metaURL} onSwitch={metaURL => setMetaURL(metaURL)} />
+		</div>
+	);
 
-    return dropdown;
+	return dropdown;
 };
 
 interface ModuleCardProps {
-    identifier: string;
-    metadata: Metadata;
-    metaURL: string;
-    setMetaURL: (metaURL: string) => void;
-    metaURLList: string[];
-    showTags: boolean;
+	identifier: string;
+	metadata: Metadata;
+	metaURL: string;
+	setMetaURL: (metaURL: string) => void;
+	metaURLList: string[];
+	showTags: boolean;
 }
 
 function fallbackImage() {
-    return (
-        <svg
-            data-encore-id="icon"
-            role="img"
-            aria-hidden="true"
-            data-testid="card-image-fallback"
-            viewBox="0 0 24 24"
-            class="fill-current"
-            style={{ width: "64px", height: "64px" }}
-        >
-            <path d="M20.929,1.628A1,1,0,0,0,20,1H4a1,1,0,0,0-.929.628l-2,5A1.012,1.012,0,0,0,1,7V22a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V7a1.012,1.012,0,0,0-.071-.372ZM4.677,3H19.323l1.2,3H3.477ZM3,21V8H21V21Zm8-3a1,1,0,0,1-1,1H6a1,1,0,0,1,0-2h4A1,1,0,0,1,11,18Z" />
-        </svg>
-    );
+	return (
+		<svg
+			data-encore-id="icon"
+			role="img"
+			aria-hidden="true"
+			data-testid="card-image-fallback"
+			viewBox="0 0 24 24"
+			class="fill-current"
+			style={{ width: "64px", height: "64px" }}
+		>
+			<path d="M20.929,1.628A1,1,0,0,0,20,1H4a1,1,0,0,0-.929.628l-2,5A1.012,1.012,0,0,0,1,7V22a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V7a1.012,1.012,0,0,0-.071-.372ZM4.677,3H19.323l1.2,3H3.477ZM3,21V8H21V21Zm8-3a1,1,0,0,1-1,1H6a1,1,0,0,1,0-2h4A1,1,0,0,1,11,18Z" />
+		</svg>
+	);
 }
 
 function titleise(name: string) {
-    return name
-        .split("-")
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(" ");
+	return name
+		.split("-")
+		.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+		.join(" ");
 }
 
 export default function ({ identifier, metadata, metaURL, setMetaURL, metaURLList, showTags }: ModuleCardProps) {
-    const { module, installed, enabled, updateEnabled, outdated, localOnly } = useModule(identifier);
-    const metaSelector = useMetaSelector({ metaURL, setMetaURL, metaURLList });
+	const { module, installed, enabled, updateEnabled, outdated, localOnly } = useModule(identifier);
+	const metaSelector = useMetaSelector({ metaURL, setMetaURL, metaURLList });
 
-    const { name, description, tags, authors, preview } = metadata;
+	const { name, description, tags, authors, preview } = metadata;
 
-    const cardClasses = S.classnames("main-card-card", {
-        // "border border-solid": installed,
-        // "border-[var(--essential-announcement)]": localOnly,
-        // "border-[var(--essential-warning)]": !localOnly && outdated,
-    });
+	const cardClasses = S.classnames("main-card-card", {
+		// "border border-solid": installed,
+		// "border-[var(--essential-announcement)]": localOnly,
+		// "border-[var(--essential-warning)]": !localOnly && outdated,
+	});
 
-    const href = metaURL.startsWith("http") ? metaURL : null;
-    const previewHref = `${metaURL}/../${preview}`;
+	const href = metaURL.startsWith("http") ? metaURL : null;
+	const previewHref = `${metaURL}/../${preview}`;
 
-    // TODO: add more important tags
-    const importantTags = [].filter(Boolean);
+	// TODO: add more important tags
+	const importantTags = [].filter(Boolean);
 
-    return (
-        <div className={cardClasses}>
-            <div className="flex flex-col h-full" draggable="true">
-                <div
-                    onClick={() => {
-                        History.push(`/marketplace/${encodeURIComponent(metaURL)}`);
-                    }}
-                    style={{ pointerEvents: "all", cursor: "pointer", marginBottom: "16px" }}
-                >
-                    <S.ReactComponents.Cards.CardImage images={[{ url: previewHref }]} FallbackComponent={fallbackImage} />
-                </div>
-                <div className="flex flex-col gap-2 flex-grow">
-                    <a
-                        draggable="false"
-                        title={name}
-                        className="hover:underline"
-                        dir="auto"
-                        href={href}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        onClick={e => e.stopPropagation()}
-                    >
-                        <div className="main-type-balladBold">{titleise(name)}</div>
-                    </a>
-                    <div className="text-sm mx-0 whitespace-normal color-[var(--spice-subtext)] flex flex-col gap-2">
-                        <AuthorsDiv authors={authors} />
-                    </div>
-                    <p className="text-sm mx-0 overflow-hidden line-clamp-3 mb-auto">{description || "No description for this package"}</p>
-                    <div className="text-[var(--spice-subtext)] whitespace-normal main-type-mestoBold">
-                        <TagsDiv tags={tags} showTags={showTags} importantTags={importantTags} />
-                    </div>
-                    <div className="flex justify-between">
-                        {metaSelector}
-                        {installed && (
-                            <S.ReactComponents.SettingToggle
-                                className="x-settings-button justify-end"
-                                value={enabled}
-                                onSelected={(checked: boolean) => {
-                                    if (checked) {
-                                        module.enable();
-                                    } else {
-                                        module.disable();
-                                    }
-                                    updateEnabled();
-                                }}
-                            />
-                        )}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={cardClasses}>
+			<div className="flex flex-col h-full" draggable="true">
+				<div
+					onClick={() => {
+						History.push(`/marketplace/${encodeURIComponent(metaURL)}`);
+					}}
+					style={{ pointerEvents: "all", cursor: "pointer", marginBottom: "16px" }}
+				>
+					<S.ReactComponents.Cards.CardImage images={[{ url: previewHref }]} FallbackComponent={fallbackImage} />
+				</div>
+				<div className="flex flex-col gap-2 flex-grow">
+					<a
+						draggable="false"
+						title={name}
+						className="hover:underline"
+						dir="auto"
+						href={href}
+						target="_blank"
+						rel="noopener noreferrer"
+						onClick={e => e.stopPropagation()}
+					>
+						<div className="main-type-balladBold">{titleise(name)}</div>
+					</a>
+					<div className="text-sm mx-0 whitespace-normal color-[var(--spice-subtext)] flex flex-col gap-2">
+						<AuthorsDiv authors={authors} />
+					</div>
+					<p className="text-sm mx-0 overflow-hidden line-clamp-3 mb-auto">{description || "No description for this package"}</p>
+					<div className="text-[var(--spice-subtext)] whitespace-normal main-type-mestoBold">
+						<TagsDiv tags={tags} showTags={showTags} importantTags={importantTags} />
+					</div>
+					<div className="flex justify-between">
+						{metaSelector}
+						{installed && (
+							<S.ReactComponents.SettingToggle
+								className="x-settings-button justify-end"
+								value={enabled}
+								onSelected={(checked: boolean) => {
+									if (checked) {
+										module.enable();
+									} else {
+										module.disable();
+									}
+									updateEnabled();
+								}}
+							/>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
