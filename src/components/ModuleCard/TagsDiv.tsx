ba3bb782780a4d9a70b219f5daf6c4cdcc7ffdb4
harbassan <harbassan@hotmail.com>
2024-03-22 16:55:44 +1300

    fix: improve tags overflow

--- src/components/ModuleCard/TagsDiv.tsx
+++ src/components/ModuleCard/TagsDiv.tsx
@@ -20,33 +20,11 @@ interface TagsDivProps {
 	showTags: boolean;
 }
 export default function ({ tags, importantTags, showTags }: TagsDivProps) {
-	const [expanded, setExpanded] = React.useState(false);
-
 	const baseTags = [importantTags, showTags && tags].flat();
 
-	let extraTags = new Array<string>();
-	// If there are more than one extra tags, slice them and add an expand button
-	if (baseTags.length - MAX_TAGS > 1) {
-		extraTags = baseTags.splice(MAX_TAGS);
-	}
-
 	return (
-		<div className="">
-			<ul className="flex flex-wrap gap-2 text-sm">
-				{baseTags.map(Tag)}
-				{expanded && extraTags.map(Tag)}
-			</ul>
-			{!expanded && extraTags.length > 0 && (
-				<button
-					className="bg-[var(--spice-tab-active)] rounded pt-0 pb-1 px-2 mt-2 border-none hover:brightness-150 focus:brightness-150"
-					onClick={e => {
-						e.stopPropagation();
-						setExpanded(true);
-					}}
-				>
-					...
-				</button>
-			)}
+		<div className="tags-wrapper">
+			<ul className="flex flex-wrap gap-2 text-sm">{baseTags.map(Tag)}</ul>
 		</div>
 	);
 }
