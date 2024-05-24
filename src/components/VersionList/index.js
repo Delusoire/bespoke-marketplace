24c1233ef7e43bdb7a2c2caedd01929a3dd29bd0
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-05-24 19:38:00 +0100

    y

--- src/components/VersionList/index.js
+++ src/components/VersionList/index.js
@@ -1,3 +1,5 @@
+import { useUpdate } from "../../util/index.js";
+import { ModuleManager } from "/hooks/protocol.js";
 import { React } from "/modules/official/stdlib/src/expose/React.js";
 import { PanelContent, PanelHeader, PanelSkeleton } from "/modules/official/stdlib/src/webpack/ReactComponents.js";
 export default function(props) {
@@ -26,6 +28,68 @@ export const VersionListContent = ({ module })=>{
             moduleInst: inst
         })));
 };
-const Version = ({ moduleInst })=>{
-    return /*#__PURE__*/ React.createElement("li", null, moduleInst.getVersion(), /*#__PURE__*/ React.createElement("button", null, "E/D"), /*#__PURE__*/ React.createElement("button", null, "I/R"));
+const Version = (props)=>{
+    return /*#__PURE__*/ React.createElement("li", null, props.moduleInst.getVersion(), /*#__PURE__*/ React.createElement(RAB, {
+        moduleInst: props.moduleInst
+    }), /*#__PURE__*/ React.createElement(DEB, {
+        moduleInst: props.moduleInst
+    }));
+};
+const RAB = (props)=>{
+    const [installed, setInstalled, updateInstalled] = useUpdate(()=>props.moduleInst.isInstalled());
+    const B = installed ? RemoveButton : AddButton;
+    return /*#__PURE__*/ React.createElement(B, {
+        ...props,
+        setInstalled: setInstalled,
+        updateInstalled: updateInstalled
+    });
+};
+const RemoveButton = (props)=>{
+    return /*#__PURE__*/ React.createElement("button", {
+        onClick: async ()=>{
+            props.setInstalled(true);
+            if (!await props.moduleInst.remove()) {
+                props.updateInstalled();
+            }
+        }
+    }, "del");
+};
+const AddButton = (props)=>{
+    return /*#__PURE__*/ React.createElement("button", {
+        onClick: async ()=>{
+            props.setInstalled(false);
+            if (!await props.moduleInst.add()) {
+                props.updateInstalled();
+            }
+        }
+    }, "ins");
+};
+const DEB = (props)=>{
+    const [enabled, setEnabled, updateEnabled] = useUpdate(()=>props.moduleInst.isEnabled());
+    const B = enabled ? DisableButton : EnableButton;
+    return /*#__PURE__*/ React.createElement(B, {
+        ...props,
+        setEnabled: setEnabled,
+        updateEnabled: updateEnabled
+    });
+};
+const DisableButton = (props)=>{
+    return /*#__PURE__*/ React.createElement("button", {
+        onClick: async ()=>{
+            props.setEnabled(true);
+            if (!await ModuleManager.disable(props.moduleInst.getModule())) {
+                props.updateEnabled();
+            }
+        }
+    }, "dis");
+};
+const EnableButton = (props)=>{
+    return /*#__PURE__*/ React.createElement("button", {
+        onClick: async ()=>{
+            props.setEnabled(false);
+            if (!await ModuleManager.enable(props.moduleInst)) {
+                props.updateEnabled();
+            }
+        }
+    }, "ena");
 };
--- src/components/VersionList/index.tsx
+++ src/components/VersionList/index.tsx
@@ -1,4 +1,6 @@
-import type { Module, ModuleInstance } from "/hooks/module.js";
+import { useUpdate } from "../../util/index.js";
+import { ModuleInstance, type Module } from "/hooks/module.js";
+import { ModuleManager } from "/hooks/protocol.js";
 import { React } from "/modules/official/stdlib/src/expose/React.js";
 import {
    PanelContent,
@@ -42,11 +44,74 @@ export const VersionListContent = ( { module }: VersionListContentProps ) => {
 interface VersionProps {
    moduleInst: ModuleInstance;
 }
-const Version = ( { moduleInst }: VersionProps ) => {
-
+const Version = ( props: VersionProps ) => {
    return <li>
-      { moduleInst.getVersion() }
-      <button>E/D</button>
-      <button>I/R</button>
+      { props.moduleInst.getVersion() }
+      <RAB moduleInst={ props.moduleInst } />
+      <DEB moduleInst={ props.moduleInst } />
    </li>;
+};
+
+interface BProps {
+   moduleInst: ModuleInstance;
+}
+
+const RAB = ( props: BProps ) => {
+   const [ installed, setInstalled, updateInstalled ] = useUpdate( () => props.moduleInst.isInstalled() );
+   const B = installed ? RemoveButton : AddButton;
+   return <B  { ...props } setInstalled={ setInstalled } updateInstalled={ updateInstalled } />;
+};
+
+interface RABProps {
+   moduleInst: ModuleInstance;
+   setInstalled: ( installed: boolean ) => void;
+   updateInstalled: () => void;
+}
+
+const RemoveButton = ( props: RABProps ) => {
+   return <button onClick={ async () => {
+      props.setInstalled( true );
+      if ( !await props.moduleInst.remove() ) {
+         props.updateInstalled();
+      }
+   } } >del</button>;
+};
+
+const AddButton = ( props: RABProps ) => {
+   return <button onClick={ async () => {
+      props.setInstalled( false );
+      if ( !await props.moduleInst.add() ) {
+         props.updateInstalled();
+      }
+   } }>ins</button>;
+};
+
+const DEB = ( props: BProps ) => {
+   const [ enabled, setEnabled, updateEnabled ] = useUpdate( () => props.moduleInst.isEnabled() );
+   const B = enabled ? DisableButton : EnableButton;
+   return <B  { ...props } setEnabled={ setEnabled } updateEnabled={ updateEnabled } />;
+};
+
+interface DEBProps {
+   moduleInst: ModuleInstance;
+   setEnabled: ( installed: boolean ) => void;
+   updateEnabled: () => void;
+}
+
+const DisableButton = ( props: DEBProps ) => {
+   return <button onClick={ async () => {
+      props.setEnabled( true );
+      if ( !await ModuleManager.disable( props.moduleInst.getModule() ) ) {
+         props.updateEnabled();
+      }
+   } }>dis</button>;
+};
+
+const EnableButton = ( props: DEBProps ) => {
+   return <button onClick={ async () => {
+      props.setEnabled( false );
+      if ( !await ModuleManager.enable( props.moduleInst ) ) {
+         props.updateEnabled();
+      }
+   } }>ena</button>;
 };
