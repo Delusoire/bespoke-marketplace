99ed3e2cf14544ea58564adf53957c6902da89ca
Delusoire <deluso7re@outlook.com>
2024-06-02 08:37:33 +0100

    y

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "https://esm.sh/i18next";
 import { initReactI18next } from "https://esm.sh/react-i18next";
 import LanguageDetector from "https://esm.sh/i18next-browser-languagedetector";
-import locales from "./resources/locales/index.js";
+import locales from "./resources/locales/index.ts";
 
 export const t = await i18n
 	.use(initReactI18next)
--- src/resources/locales/index.ts
+++ src/resources/locales/index.ts
@@ -1,5 +1,5 @@
 import type { ResourceLanguage } from "https://esm.sh/v135/i18next";
-import { fetchJSON } from "/hooks/util.js";
+import { fetchJSON } from "/hooks/util.ts";
 
 export default {
 	en: await fetchJSON<ResourceLanguage>("/modules/Delusoire/marketplace/src/resources/locales/en.json"),
